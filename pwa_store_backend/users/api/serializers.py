6aa600e419a09fe8e9afda1b25bc298399352c12
Nathan Foster <nfoster@rnaker.com>
2021-08-19 08:13:45 -0700

    Need to serialize

--- pwa_store_backend/users/api/serializers.py
+++ pwa_store_backend/users/api/serializers.py
@@ -1,21 +1,21 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from pwa_store_backend.users.models import UserSetting
+from pwa_store_backend.users.models import User, UserSetting
 
-User = get_user_model()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["username", "name", "url"]
-
-        extra_kwargs = {
-            "url": {"view_name": "api:user-detail", "lookup_field": "username"}
-        }
-
+# User = get_user_model()
 
 class UserSettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserSetting
         fields = ["mode"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    settings = UserSettingSerializer()
+    class Meta:
+        model = User
+        fields = ("username", "name", "url", "settings")
+
+        extra_kwargs = {
+            "url": {"view_name": "api:user-detail", "lookup_field": ("id", "username")}
+        }
--- pwa_store_backend/users/api/views.py
+++ pwa_store_backend/users/api/views.py
@@ -2,9 +2,8 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import  ModelViewSet
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from django.contrib.auth.models import update_last_login
@@ -14,7 +13,7 @@ from .serializers import UserSerializer, UserSettingSerializer
 User = get_user_model()
 
 
-class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = "username"
