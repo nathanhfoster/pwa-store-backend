1e365ea5b5c402c119f2b440662a6f8320f7012b
Nathan Foster <nfoster@rnaker.com>
2021-08-31 09:28:52 -0700

    Auto stash before merge of "master" and "origin/master"

--- pwa_store_backend/pwas/admin_forms.py
+++ pwa_store_backend/pwas/admin_forms.py
@@ -7,11 +7,11 @@ class PwaAdminForm(forms.ModelForm):
         model = Pwa
         fields = '__all__'
 
-    def __init__(self, *args, **kwargs):
-        instance = kwargs.get('instance')
-        if instance and instance.manifest_json:
-            instance.manifest_json = json.loads(instance.manifest_json)
-        super().__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     instance = kwargs.get('instance')
+    #     if instance and instance.manifest_json:
+    #         instance.manifest_json = json.loads(instance.manifest_json)
+    #     super().__init__(*args, **kwargs)
 
     def clean_manifest_json(self):
         manifest_json = self.cleaned_data['manifest_json']
