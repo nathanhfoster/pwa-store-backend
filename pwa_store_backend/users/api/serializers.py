715c87d4c318ee3810f88243baafdc13780af3be
Nathan Foster <nfoster@rnaker.com>
2021-10-07 17:03:28 -0700

    Can't update username

--- pwa_store_backend/users/api/serializers.py
+++ pwa_store_backend/users/api/serializers.py
@@ -27,7 +27,6 @@ class UserSerializer(ModelSerializer):
         model = User
         fields = ("id", "name", 'email', "setting", "user_favorites", "username", "password",)
         extra_kwargs = {
-            'username': {'write_only': True},
             'password': {'write_only': True},
         }
 
